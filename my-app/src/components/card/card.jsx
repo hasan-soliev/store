9909b802213cdb4e3fdea628d87a4a374d0cc144
Soliev Hasan <thrsoliev@gmail.com>
2024-09-18 15:55:23 +0500

    add button and tag p

--- my-app/src/components/card/card.jsx
+++ my-app/src/components/card/card.jsx
@@ -7,6 +7,8 @@ export default function Card({product,addToCart}) {
     <img src={img} className="img" />
     <p className="text name">Company: {name}</p>
     <p className="text model">Model :{model}</p>
+    <p className="text price">Price: {price} $</p>
+    <button className="btn" onClick={() => addToCart(product)}>Add to cart</button>
       </div>
   );
 }
